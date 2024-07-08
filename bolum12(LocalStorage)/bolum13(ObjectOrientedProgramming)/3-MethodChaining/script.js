16235d9e73321b2abcc5efd658cb86d78af369de
DevTelmanAbdulla <147204762+DevTelmanAbdulla@users.noreply.github.com>
2024-07-08 10:52:15 +0400

    Add files via upload

---
+++ bolum12(LocalStorage)/bolum13(ObjectOrientedProgramming)/3-MethodChaining/script.js
@@ -0,0 +1,26 @@
+class User{
+    constructor(firstName,email){
+      this.name = firstName;
+      this.email = email;
+    }
+    //login bir funksiya olduqu ucun constructorun icersinde tamamlanmalidir
+    login(){
+        console.log(`${this.name} logged in`);
+        return this; //we used template strings
+    }
+    logout(){
+        console.log(`${this.name} logged out`);
+        // return this;  bunu yazmaqa ehtiyac yoxdur cunki en sonda logout calisir
+    }
+}
+
+const userOne = new User('Scarlett','scarlett@hotmail.com');
+const userTwo = new User('Megan','megan@gmail.com');
+// console.log(userOne,userTwo);
+// console.log(userOne.login(),userTwo.logout());
+console.log(userOne.login().logout());
+
+
+
+
+
